8bf587dd4710bbc6e0c8c336679deef69e139446
Fahima-anny <fahima.info03@gmail.com>
2024-11-23 18:16:46 +0600

    server created

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express') ;
+const app = express() ;
+const phones = require('./phones.json')
+const port = 5000 ;
+
+app.get('/', (req, res) => {
+    res.send('My Phones Server created') ;
+})
+
+app.get('/phones', (req, res) => {
+    res.send(phones) ;
+})
+
+app.get('/phones/:id', (req, res) => {
+    const id = parseInt(req.params.id) ;
+    console.log(`give me data of id: ${id}`)
+    const phone = phones.find(p => p.id === id ) || {}
+res.send(phone)
+})
+
+app.listen(port, () => {
+    console.log("Server running at port : ", port) ;
+})
